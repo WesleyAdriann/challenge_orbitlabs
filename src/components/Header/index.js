06393d94c45572a5730340608207262837a61943
Wesley Souza <wesleyadriann@gmail.com>
2020-01-30 17:04:27 -0300

    Adds vector icons
    Signed-off-by: Wesley Souza <wesleyadriann@gmail.com>

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-import {Container, Title} from './style';
+import {Container, Title, OcticonsStyled} from './style';
 
 const Header = () => (
   <Container colors={['#D06600', '#E69A09']}>
+    <MaterialIcons name="menu" size={40} color="#FFF" />
     <Title>Restaurantes</Title>
+    <OcticonsStyled name="settings" size={36} color="#FFF" />
   </Container>
 );
 
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
+import Octicons from 'react-native-vector-icons/Octicons';
 
 export const Container = styled(LinearGradient)`
   height: 80px;
   justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
   /* align-items: space; */
 `;
 
@@ -11,4 +15,8 @@ export const Title = styled.Text`
   font-family: 'Roboto';
   color: #fff;
   font-size: 26px;
+`;
+
+export const OcticonsStyled = styled(Octicons)`
+  transform: rotate(90deg);
 `;
