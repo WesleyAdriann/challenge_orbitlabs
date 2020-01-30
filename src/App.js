6088723fed770884f35daf88f1a688f0ef26b9aa
Wesley Souza <wesleyadriann@gmail.com>
2020-01-30 15:19:22 -0300

    Configures routes
    Signed-off-by: Wesley Souza <wesleyadriann@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {View, Text} from 'react-native';
-
 import 'react-native-gesture-handler';
 
 import store from './store';
 
+import Routes from './routes';
+
 const App = () => (
   <Provider store={store}>
-    <View>
-      <Text>init</Text>
-    </View>
+    <Routes />
   </Provider>
 );
 
---
+++ src/routes.js
@@ -0,0 +1,48 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import Login from './views/Login';
+
+import Home from './views/Home';
+
+const AuthStack = createStackNavigator(
+  {
+    Login: {
+      screen: Login,
+      path: 'Login',
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: 'Login',
+  },
+);
+
+const AppStack = createStackNavigator(
+  {
+    Home: {
+      screen: Home,
+      path: 'Home',
+      navigationOptions: {
+        headerShown: false,
+      },
+    },
+  },
+  {
+    initialRouteName: 'Home',
+  },
+);
+
+const Routes = createSwitchNavigator(
+  {
+    AuthStack,
+    AppStack,
+  },
+  {
+    initialRouteName: 'AuthStack',
+  },
+);
+
+export default createAppContainer(Routes);
